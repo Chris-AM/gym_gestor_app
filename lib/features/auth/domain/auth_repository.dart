f08caea11e4a9891e5ff8818903e4bd71d8cfd60
Chris-Am <c.aranguizm@outlook.com>
2023-11-04 01:12:27 -0300

    :sparkles: declaring auth domain

--- lib/features/auth/domain/auth_repository.dart
+++ lib/features/auth/domain/auth_repository.dart
@@ -1,6 +1,6 @@
 import '/domain/domain.dart';
 
-abstract class AuthDataSource {
+abstract class AuthRepository {
   Future<UserEntity> login(String email, String password);
   Future<UserEntity> register(String email, String password);
   Future<UserEntity> checkAuth(String token);
---
+++ lib/features/auth/domain/domain.dart
@@ -0,0 +1,2 @@
+export './auth_datasource.dart';
+export './auth_repository.dart';
